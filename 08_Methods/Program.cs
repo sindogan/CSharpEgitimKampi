eba86975f97a39a5f210d37e7414efa1574b2410
Sinan Dogan <sinandogan360@gmail.com>
2025-08-09 15:38:32 +0300

    8. video ended

---
+++ 08_Methods/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region  Void Metotlar
+            //() metot ifade eder 
+            //geriye değer döndürmeyen metotlar
+            //Void geriye değer döndürmeyen
+            //void CustomerList()
+            //{
+            //    Console.WriteLine("Ali Yıldız");
+            //    Console.WriteLine("Ayşe Yıldız");
+            //    Console.WriteLine("Ali Yılmaz");
+            //    Console.WriteLine("Veli Yıldız");
+
+            //}
+
+            //CustomerList();
+            //void Sum()
+            //{
+            //    int x = 1;
+            //    int y = 2;
+            //    int z = x + y;
+            //    Console.WriteLine(z);
+            //}
+            //Sum();
+
+            #endregion
+            #region Geriye Değer Döndürmeyen Parametreli Metotlar
+            //void WriteMethod(string customerName)
+            //{
+            //    Console.WriteLine(customerName);
+
+            //}
+            //WriteMethod("Sinan Doğan");
+
+            //void CustomerCard(string name,string surname)
+            //{
+            //    Console.WriteLine("Müşteri: " +  name + " " + surname);
+            //}
+
+            //CustomerCard("sinan", "doğan");
+
+            #endregion
+            #region Geriye Değer Döndürmeyen Int Parametreli Metotlar
+            //void Sum(int number1,int number2,int number3)
+            //{
+            //    int result = number1 + number2 + number3;
+            //    Console.WriteLine(result);
+            //}
+            //Sum(2, 5, 6);
+
+
+
+
+
+            #endregion
+            #region Geriye Değer Döndüren Metotlar
+            // string CustomerName()
+            //{
+            //    return "Buse Yıldız";
+            //}
+            //CustomerName();
+            //string StudentCard()
+            //{
+            //    string name = "ali";
+            //    string surname = "yıldız";
+            //    return name + " " + surname;
+            //}
+            //Console.WriteLine(StudentCard());
+
+
+
+
+
+            #endregion
+            #region Geriye Değer Döndüren String Parametreli Metotlar
+            //string CountryCard(string countryName,string capital,string flagColor)
+            //{
+            //    string cardInfo = "Ülke: " + countryName + "- Başkent: " + capital + " - Bayrak Rengi: " + flagColor;
+            //    return cardInfo;
+            //}
+            //string x, y, z;
+            //Console.Write("ülke Adını Giriniz: ");
+            //x = Console.ReadLine();
+
+            //Console.Write("Başkent Adını Giriniz: ");
+            //y = Console.ReadLine();
+
+            //Console.Write("Bayrak renklerini Giriniz: ");
+            //z = Console.ReadLine();
+
+            //Console.WriteLine(CountryCard(x,y,z));
+
+            #endregion
+            #region Geriye Değer Döndüren Int Parametreli Metotlar
+
+            #endregion
+            Console.Read();
+        }
+    }
+}
